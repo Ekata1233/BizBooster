c7a439012bc6417ffd0f3e00f2d006a90c441524
Ekata1233 <jadhavjanki8464@gmail.com>
2025-05-07 14:43:57 +0530

    put and delete comment

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,12 +1,17 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import User from '@/models/User';
 import { connectToDatabase } from '@/utils/db';
 
-// GET user by ID
-export const GET = async (_req: Request, context: { params: { id: string } }) => {
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export const GET = async (_req: NextRequest, { params }: Context) => {
   try {
     await connectToDatabase();
-    const user = await User.findById(context.params.id);
+    const user = await User.findById(params.id);
     if (!user) return NextResponse.json({ error: 'User not found' }, { status: 404 });
     return NextResponse.json(user, { status: 200 });
   } catch (error: unknown) {
@@ -15,12 +20,11 @@ export const GET = async (_req: Request, context: { params: { id: string } }) =>
   }
 };
 
-// UPDATE user by ID
-export const PUT = async (req: Request, context: { params: { id: string } }) => {
+export const PUT = async (req: NextRequest, { params }: Context) => {
   try {
     await connectToDatabase();
     const body = await req.json();
-    const updatedUser = await User.findByIdAndUpdate(context.params.id, body, { new: true });
+    const updatedUser = await User.findByIdAndUpdate(params.id, body, { new: true });
     if (!updatedUser) return NextResponse.json({ error: 'User not found' }, { status: 404 });
     return NextResponse.json({ success: true, user: updatedUser }, { status: 200 });
   } catch (error: unknown) {
@@ -29,11 +33,10 @@ export const PUT = async (req: Request, context: { params: { id: string } }) => 
   }
 };
 
-// DELETE user by ID
-export const DELETE = async (_req: Request, context: { params: { id: string } }) => {
+export const DELETE = async (_req: NextRequest, { params }: Context) => {
   try {
     await connectToDatabase();
-    const deletedUser = await User.findByIdAndDelete(context.params.id);
+    const deletedUser = await User.findByIdAndDelete(params.id);
     if (!deletedUser) return NextResponse.json({ error: 'User not found' }, { status: 404 });
     return NextResponse.json({ success: true, message: 'User deleted' }, { status: 200 });
   } catch (error: unknown) {
