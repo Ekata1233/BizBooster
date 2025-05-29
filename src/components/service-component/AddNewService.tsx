38daa5852e4c52ac534c211020ceedcd9caa7ffb
Ekata1233 <jadhavjanki8464@gmail.com>
2025-05-29 11:58:19 +0530

    vercel error fix

--- src/components/service-component/AddNewService.tsx
+++ src/components/service-component/AddNewService.tsx
@@ -6,14 +6,6 @@ import BasicDetailsForm from './BasicDetailsForm';
 import FranchiseDetailsForm from './FranchiseDetailsForm';
 
 
-type FAQ = {
-  question: string;
-  answer: string;
-};
-
-interface WhyChoose {
-  _id?: string;
-}
 
 
 type RowData = {
