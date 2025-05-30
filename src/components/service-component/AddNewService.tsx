39fce2be92df976ddd288b95671d101c856ede07
aniket-patil7075 <akp7075@gmail.com>
2025-05-30 12:07:13 +0530

    change in the ckeditor

--- src/components/service-component/AddNewService.tsx
+++ src/components/service-component/AddNewService.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState } from 'react';
 import ComponentCard from '../common/ComponentCard';
 import ServiceDetailsForm, { ServiceDetails } from './ServiceDetailsForm';
