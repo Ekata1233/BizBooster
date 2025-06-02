8a8f08876f4738e84d77789df13ac680ac87607e
aniket-patil7075 <akp7075@gmail.com>
2025-06-02 17:51:15 +0530

    add zone pending

---
+++ src/app/(admin)/zone-management/zone-list/page.tsx
@@ -0,0 +1,16 @@
+import ComponentCard from '@/components/common/ComponentCard'
+import PageBreadcrumb from '@/components/common/PageBreadCrumb'
+import React from 'react'
+
+const ZoneList = () => {
+  return (
+    <div>
+         <PageBreadcrumb pageTitle="Zone List" />
+         <ComponentCard title="Zone List">
+            <div></div>
+        </ComponentCard>
+    </div>
+  )
+}
+
+export default ZoneList
