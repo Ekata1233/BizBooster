52abc3c8e74d62de90c32484327f8cb5c3f0287e
aniket-patil7075 <akp7075@gmail.com>
2025-07-19 16:32:20 +0530

    add gallery images in provide

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.{js,ts,jsx,tsx}', // ✅ tells Tailwind where to scan
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
