f275a71d167c5d20a2cab569717cecb5c4c823b7
aniket-patil7075 <akp7075@gmail.com>
2025-07-19 16:42:33 +0530

    add gallery images in provide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
+      <body>
         <RouteLoader />
 
         <ThemeProvider>
