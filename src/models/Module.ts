d09e927d3b2319b2f8ff3079e377aa3c3377fecd
Ekata1233 <jadhavjanki8464@gmail.com>
2025-07-25 10:38:15 +0530

    solve basic price and commision

--- src/models/Module.ts
+++ src/models/Module.ts
@@ -5,7 +5,6 @@ const moduleSchema = new mongoose.Schema(
     name: { type: String, required: true },
     image: { type: String },
     sortOrder: { type: Number, default: 0 },
-    isDeleted: { type: Boolean, default: false }, // Soft delete flag
   },
   { timestamps: true }
 );
