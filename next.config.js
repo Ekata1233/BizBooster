889828ab9aaadfab18fe9bf1e6e335fcd4b794fd
aniket-patil7075 <akp7075@gmail.com>
2025-08-04 10:34:30 +0530

    change in global invoice

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['ik.imagekit.io'],
+//     unoptimized: true,
+//   },
+//   output: 'standalone',
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     });
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -6,10 +25,16 @@ const nextConfig = {
   },
   output: 'standalone',
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.map$/,
+        use: 'ignore-loader',
+      }
+    );
     return config;
   },
 };
