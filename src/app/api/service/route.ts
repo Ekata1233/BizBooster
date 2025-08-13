91485d743aa7f30658cfdbad88995161008f2a0d
Ekata1233 <jadhavjanki8464@gmail.com>
2025-08-13 15:25:02 +0530

    provider mrp and provider discount add

--- src/app/api/service/route.ts
+++ src/app/api/service/route.ts
@@ -297,7 +297,7 @@ export async function GET(req: NextRequest) {
       .populate({
         path: 'providerPrices.provider',
         model: 'Provider',
-        select: 'storeName',
+        select: 'storeInfo.storeName',
       })
       .sort(sortOption)
       .exec();
