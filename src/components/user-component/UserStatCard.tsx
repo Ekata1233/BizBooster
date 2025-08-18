388e5facf25c8bfb98bf91acd9d4a8f9785c64a6
aniket-patil7075 <akp7075@gmail.com>
2025-08-18 15:39:34 +0530

    GP and Non GP are added

--- src/components/user-component/UserStatCard.tsx
+++ src/components/user-component/UserStatCard.tsx
@@ -12,45 +12,44 @@ import { useUserContext } from '@/context/UserContext';
 const UserStatCard = () => {
     const { users } = useUserContext();
 
+    console.log("users data : ", users)
+
+
     if (!users) {
         return <div>Loading...</div>;
     }
+
+    const gpCount = users.filter(user => user.packageActive === true).length;
+    const nonGpCount = users.filter(user => user.packageActive === false).length;
     return (
         <div>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 md:gap-6 my-5">
-            <StatCard
-                title="Total Users"
-                value={users.length}
-                icon={UserIcon}
-                badgeColor="success"
-                badgeValue="6.88%"
-                badgeIcon={ArrowUpIcon}
-            />
-            <StatCard
-                title="Total Booking"
-                value="20"
-                icon={CalenderIcon}
-                badgeColor="success"
-                badgeValue="6.88%"
-                badgeIcon={ArrowUpIcon}
-            />
-            <StatCard
-                title="Total Revenue"
-                value="₹8420"
-                icon={DollarLineIcon}
-                badgeColor="success"
-                badgeValue="6.88%"
-                badgeIcon={ArrowUpIcon}
-            />
-            <StatCard
-                title="Revenue"
-                value="₹8420"
-                icon={BoxCubeIcon}
-                badgeColor="success"
-                badgeValue="6.88%"
-                badgeIcon={ArrowUpIcon}
-            />
-        </div>
+                <StatCard
+                    title="Total Users"
+                    value={users.length}
+                    icon={UserIcon}
+                    badgeColor="success"
+                    badgeValue="6.88%"
+                    badgeIcon={ArrowUpIcon}
+                />
+                <StatCard
+                    title="GP"
+                    value={gpCount}
+                    icon={CalenderIcon}
+                    badgeColor="success"
+                    badgeValue="6.88%"
+                    badgeIcon={ArrowUpIcon}
+                />
+                <StatCard
+                    title="Non GP"
+                    value={nonGpCount}
+                    icon={DollarLineIcon}
+                    badgeColor="success"
+                    badgeValue="6.88%"
+                    badgeIcon={ArrowUpIcon}
+                />
+
+            </div>
         </div>
     )
 }
