413d275481da93ad8b161d7b423866a2abcb0cf4
aniket-patil7075 <akp7075@gmail.com>
2025-08-19 14:20:44 +0530

    add employee frontend desing and api done

---
+++ src/app/(admin)/employee-management/employee-role/page.tsx
@@ -0,0 +1,18 @@
+import ComponentCard from '@/components/common/ComponentCard'
+import PageBreadcrumb from '@/components/common/PageBreadCrumb'
+import React from 'react'
+
+const page = () => {
+  return (
+   <div>
+      <PageBreadcrumb pageTitle="Employee Role" />
+      <div className="space-y-6">
+        <ComponentCard title="Create new Role">
+          <div></div>
+        </ComponentCard>
+      </div>
+    </div>
+  )
+}
+
+export default page
