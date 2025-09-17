9542142bdc0f130351b5f47cfa402affe7663065
Ekata1233 <jadhavjanki8464@gmail.com>
2025-09-17 13:08:16 +0530

    add deploy.yml file

--- src/components/ecommerce/EcommerceMetrics.tsx
+++ src/components/ecommerce/EcommerceMetrics.tsx
@@ -39,7 +39,7 @@ export const EcommerceMetrics = () => {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 md:gap-6">
       <MetricCard
-        title="Users"
+        title="Users11"
         value={activeUsers.length}
         trend="up"
         icon={<GroupIcon className="text-gray-800 size-6 dark:text-white/90" />}
