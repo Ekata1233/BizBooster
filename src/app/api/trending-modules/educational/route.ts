41eb69e2eec9193021fdc59e21d73aa4a4125e06
Ekata1233 <jadhavjanki8464@gmail.com>
2025-09-25 12:22:43 +0530

    franchaise, marketing, educational api done, isTrending: true

--- src/app/api/trending-modules/educational/route.ts
+++ src/app/api/trending-modules/educational/route.ts
@@ -6,7 +6,7 @@ const EDUCATIONAL_MODULE_ID = "6822e02de8235364b35df1ae";
 
 export async function GET() {
   await connectToDatabase();
-  const services = await TrendingModuleService.find({ moduleId: EDUCATIONAL_MODULE_ID })
+  const services = await TrendingModuleService.find({ moduleId: EDUCATIONAL_MODULE_ID, isTrending: true  })
     .populate("serviceId")
     .exec();
   return NextResponse.json(services);
