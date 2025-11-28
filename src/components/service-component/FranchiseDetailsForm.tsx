7e42c53459c9b3443e3d39a5cbd2836b2ea8e62e
aniket-patil7075 <akp7075@gmail.com>
2025-11-28 17:00:03 +0530

    changes in update service

--- src/components/service-component/FranchiseDetailsForm.tsx
+++ src/components/service-component/FranchiseDetailsForm.tsx
@@ -117,6 +117,33 @@ const FranchiseDetailsForm: React.FC<FranchiseDetailsFormProps> = ({ data, setDa
   ]);
 
 
+  useEffect(() => {
+     if (data?.termsAndConditions) {
+    setTermsAndConditions(data.termsAndConditions);
+  }
+  if (data.commission) {
+    const numericValue = data.commission.replace(/[^\d]/g, '');
+    setCommissionValue(numericValue);
+  }
+   if (Array.isArray(data.investmentRange) && data.investmentRange.length) {
+    setInvestmentRange(data.investmentRange);
+  }
+
+  if (
+    Array.isArray(data.monthlyEarnPotential) &&
+    data.monthlyEarnPotential.length
+  ) {
+    setMonthlyEarnPotential(data.monthlyEarnPotential);
+  }
+
+  if (Array.isArray(data.franchiseModel) && data.franchiseModel.length) {
+    setFranchiseModel(data.franchiseModel);
+  }
+
+  if (Array.isArray(data.extraSections) && data.extraSections.length) {
+    setExtraSections(data.extraSections);
+  }
+}, [data]);
   // -----------------------
   // Commission handlers (kept unchanged)
   // -----------------------
