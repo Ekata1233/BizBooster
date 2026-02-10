9e719b379007bbb35f1af5ca18e4f2b3769cf65e
Ekata1233 <jadhavjanki8462@gmail.com>
2026-02-10 11:13:36 +0530

    provider login issue

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,30 +1,4 @@
-// import Zone from '@/models/Zone';
-// import mongoose from 'mongoose';
 
-// const MONGODB_URI = process.env.MONGO_URI || 'mongodb+srv://ftfltechnology:ftfltechnology@bizboostercluster.o31hnq3.mongodb.net/?retryWrites=true&w=majority&appName=BizBoosterCluster';
-
-// let isConnected = false;
-
-// const connectToDatabase = async () => {
-//   if (isConnected) {
-//     console.log('Already connected to MongoDB');
-//     return;
-//   }
-
-//   try {
-//     // Connect to MongoDB without deprecated options
-//     await mongoose.connect(MONGODB_URI, {
-//       serverSelectionTimeoutMS: 5000, // fail fast if cannot connect
-//       ssl: true, // force TLS
-//     });
-//     isConnected = true;
-//     console.log('Connected to MongoDB');
-//     await ensurePanIndiaZone();
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//     process.exit(1);
-//   }
-// };
 
 // export { connectToDatabase };
 import mongoose from "mongoose";
